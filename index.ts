aee1504e52dc8e4c32ecd2722822a80133160cba
reti.daniel96@gmail.com <reti.daniel96@gmail.com>
2022-04-04 21:52:55 +0200

    Deploy to heroku by github actions
    Updated package.json
    
    Create node.js.yml
    
    update node.js.yml
    
    update on package.json
    
    update yml
    
    test
    
    update yml
    
    test
    
    test
    
    test
    
    test2
    
    Heroku
    
    Heroku
    
    Heroku
    
    Heroku
    
    Heroku
    
    Heroku
    
    Heroku
    
    Heroku
    
    commit
    
    update actions
    
    addon update
    
    asd
    
    please work
    
    now
    
    ?
    
    lol
    
    jetz
    
    ok
    
    fnally
    
    back like
    
    hmm
    
    now
    
    Heroku
    
    changes
    
    letsgoo bébi
    
    now
    
    ok
    
    now
    
    fuuuck
    
    test
    
    asd

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world and bye!!");
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+console.log("asd");
