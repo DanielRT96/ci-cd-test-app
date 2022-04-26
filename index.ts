3a74a2b1a908ccddb630393649df159c5d4a3228
reti.daniel96@gmail.com <reti.daniel96@gmail.com>
2022-04-26 18:29:22 +0200

    Change text

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world and bye!! - circle");
+  res.send("This was deployed by CircleCI");
 });
 
 app.listen(port, () => {
