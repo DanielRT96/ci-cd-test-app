66414bfb9fe2620ff10d0933859c92c7613c683a
reti.daniel96@gmail.com <reti.daniel96@gmail.com>
2022-06-20 23:49:30 +0200

    lolsyy

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.send('fucking hell!!!')
+  res.send('is it fixed?!!')
 })
 
 app.listen(port, () => {
