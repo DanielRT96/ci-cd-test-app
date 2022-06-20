5baacf9feb93204553ea045dbd3614214ee11036
reti.daniel96@gmail.com <reti.daniel96@gmail.com>
2022-06-21 00:30:56 +0200

    asd

--- index.ts
+++ index.ts
@@ -3,10 +3,9 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.send('Sziasztok!!')
+  res.send('Sziasztok!!!')
 })
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
 })
-console.log('asd')
